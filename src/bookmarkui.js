2ea8ea02b8404427dd4003a48223b727f2bc002d
Rasmus Rummel <rasmusrummel@gmail.com>
2019-08-28 17:24:13 +0700

    Update bookmarkui.js

--- src/bookmarkui.js
+++ src/bookmarkui.js
@@ -5,7 +5,7 @@ import ClickObserver from '@ckeditor/ckeditor5-engine/src/view/observer/clickobs
 import ContextualBalloon from '@ckeditor/ckeditor5-ui/src/panel/balloon/contextualballoon';
 import clickOutsideHandler from '@ckeditor/ckeditor5-ui/src/bindings/clickoutsidehandler';
 
-import ViewPopup from './ui/viewpopup'; // maybe DisplayView (not ViewView)
+import ViewPopup from './ui/viewpopup';
 import EditPopup from './ui/editpopup';
 import bookmarkIcon from '../theme/icons/bookmark.svg';
 
@@ -28,7 +28,7 @@ export default class LinkUI extends Plugin {
         this._viewPopup = this._createViewPopup();
         this._editPopup = this._createEditPopup();
 
-        editor.ui.componentFactory.add('bookmark', locale => { // name to put into the config.toolbar
+        editor.ui.componentFactory.add('bookmark', locale => {
             const btn = new ButtonView(locale);
             btn.set({
                 label: editor.t('Bookmark'),
@@ -37,10 +37,8 @@ export default class LinkUI extends Plugin {
                 icon: bookmarkIcon
             });
 
-            // Bind button to the command.
             const bookmarkCommand = editor.commands.get('bookmark');
             btn.bind('isEnabled').to(bookmarkCommand, 'isEnabled');
-            //btn.bind('isOn').to(bookmarkCommand, 'value', value => !!value); // command only have value if the name-attribute has a value, however it is valid to have an empty value - so we cannot bind this easy
             btn.bind('isOn').to(bookmarkCommand, 'isBookmark');
 
             this.listenTo(btn, 'execute', () => {
@@ -51,7 +49,6 @@ export default class LinkUI extends Plugin {
             return btn;
         });
 
-        // Attach lifecycle actions to the the balloon.
         this._enableUserBalloonInteractions();
     }
 
@@ -62,11 +59,11 @@ export default class LinkUI extends Plugin {
         const command = editor.commands.get('bookmark');
         viewPopup.lblName.bind('text').to(command, 'value');
 
-        this.listenTo(viewPopup, 'edit', () => { // then the viewPopup.editButtonView is clicked it will fire 'edit' on the viewPopup
+        this.listenTo(viewPopup, 'edit', () => {
             this._balloon.remove(this._viewPopup);
             this._balloon.add({
                 view: this._editPopup,
-                position: this._getBalloonPositionData() // returns a DOM range
+                position: this._getBalloonPositionData()
             });
 
             this._editPopup.tbName.select();
@@ -92,20 +89,17 @@ export default class LinkUI extends Plugin {
         const command = editor.commands.get('bookmark');
         editPopup.tbName.bind('value').to(command, 'value');
 
-        editPopup.keystrokes.set('Esc', (data,  cancel) => {
+        editPopup.keystrokes.set('Esc', (data, cancel) => {
             this._hideUI();
             cancel();
         });
 
-        // Execute link command after clicking the "Save" button.
         this.listenTo(editPopup, 'submit', () => {
-            //const bookmarkName = editPopup.tbName.inputView.element.value;
             const bookmarkName = editPopup.tbName.element.value;
             editor.execute('bookmark', bookmarkName);
             this._hideUI();
         });
 
-        // Hide the panel after clicking the "Cancel" button.
         this.listenTo(editPopup, 'cancel', () => {
             this._hideUI();
         });
@@ -116,8 +110,6 @@ export default class LinkUI extends Plugin {
     _enableUserBalloonInteractions() {
         const viewDocument = this.editor.editing.view.document;
 
-        // Handle click on view document and show panel when selection is placed inside the math element.
-        // Keep panel open until selection will be inside the same math element.
         this.listenTo(viewDocument, 'click', () => {
             const elmBookmark = this._getSelectedBookmarkElement();
             if (elmBookmark) {
@@ -125,19 +117,16 @@ export default class LinkUI extends Plugin {
             }
         });
 
-        // Close the panel on the Esc key press when the editable has focus and the balloon is visible.
         this.editor.keystrokes.set('Esc', (data, cancel) => {
             if (this._balloon.hasView(this._viewPopup) || this._balloon.hasView(this._editPopup)) {
-            //if (this._balloon.visibleView === this._editPopup) { // () => this._isPopupVisible
                 this._hideUI();
                 cancel();
             }
         });
 
-        // Close on click outside of balloon panel element.
-        clickOutsideHandler({ // https://ckeditor.com/docs/ckeditor5/latest/api/module_ui_bindings_clickoutsidehandler.html#static-function-clickOutsideHandler
+        clickOutsideHandler({
             emitter: this._editPopup,
-            activator: () => this._balloon.visibleView === this._editPopup || this._balloon.visibleView == this._viewPopup,// () => this._isPopupVisible
+            activator: () => this._balloon.visibleView === this._editPopup || this._balloon.visibleView == this._viewPopup,
             contextElements: [this._balloon.view.element],
             callback: () => this._hideUI()
         });
@@ -148,7 +137,7 @@ export default class LinkUI extends Plugin {
         const selection = view.document.selection;
 
         var elm = selection.getSelectedElement();
-        if (elm && elm.is('containerElement')) {  // on the View, bookmark is a containerElement (while on the Model, bookmark is an Element)
+        if (elm && elm.is('containerElement')) {
             const customBookmarkProperty = !!elm.getCustomProperty('bookmarkName');
             if (customBookmarkProperty) {
                 return elm;
@@ -158,7 +147,6 @@ export default class LinkUI extends Plugin {
 
     _showUI() {
         const editor = this.editor;
-        const command = editor.commands.get('bookmark');
 
         const elmBookmark = this._getSelectedBookmarkElement();
         if (!elmBookmark) {
@@ -202,32 +190,22 @@ export default class LinkUI extends Plugin {
         }
 
         if (this._balloon.hasView(this._editPopup)) {
-            this._editPopup.saveButtonView.focus(); // some browsers have problems then removing a textbox with focus from DOM (https://github.com/ckeditor/ckeditor5/issues/1501)
-            //this._editPopup.cancelButtonView.blur();
+            this._editPopup.saveButtonView.focus();
             this._balloon.remove(this._editPopup);
         }
 
-        this.editor.editing.view.focus(); // the editPopup has a tbName (though actually saveButtonView) with focus, then removing the editPopup focus is lost - bring focus back to the editing view.
+        this.editor.editing.view.focus();
     }
 
     _addPopupViewDELETE() {
-        if (this._balloon.hasView(this._editPopup)) { // if (this._isPopupInPanel || MATHCONFIG.ENABLED === false) {
+        if (this._balloon.hasView(this._editPopup)) {
             return;
         }
-        //const positionData = this._getBalloonPositionData();
-        this._balloon.add({ // https://ckeditor.com/docs/ckeditor5/latest/api/module_ui_panel_balloon_contextualballoon-ContextualBalloon.html#function-add
+
+        this._balloon.add({
             view: this._editPopup,
-            position: this._getBalloonPositionData() // returns a DOM range
+            position: this._getBalloonPositionData()
         });
-        //this._editPopup.update();
-
-
-        //this.listenTo(this.editor.editing.view, 'render', () => {
-        //    console.log("render");
-        //    if (this._isPopupVisible) {
-        //        // In SyntaxerPro I should re-syntax the code content here
-        //    }
-        //});
     }
 
     _getBalloonPositionData() {
@@ -236,9 +214,7 @@ export default class LinkUI extends Plugin {
         const targetLink = this._getSelectedBookmarkElement();
 
         const target = targetLink ?
-            // When selection is inside link element, then attach panel to this element.
             view.domConverter.mapViewToDom(targetLink) :
-            // Otherwise attach panel to the selection.
             view.domConverter.viewRangeToDom(viewDocument.selection.getFirstRange());
 
         return { target };
