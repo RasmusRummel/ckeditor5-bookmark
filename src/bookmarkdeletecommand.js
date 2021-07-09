3dc6e1a8b4807444a316e46921a3af1e74b81f35
Rasmus Rummel <rasmusrummel@gmail.com>
2021-07-09 21:37:57 +0700

    Update bookmarkdeletecommand.js

--- src/bookmarkdeletecommand.js
+++ src/bookmarkdeletecommand.js
@@ -1,6 +1,6 @@
 import Command from '@ckeditor/ckeditor5-core/src/command';
 
-export default class BookmarkDeleteCommand extends Command {
+export default class DeleteBookmark extends Command {
     execute() {
         const editor = this.editor;
         const modelSelection = this.editor.model.document.selection;
@@ -9,18 +9,14 @@ export default class BookmarkDeleteCommand extends Command {
             if (modelSelection.isCollapsed) {
                 return;
             }
-            else { // called from bookmarkui._createViewPopup then the viewPopup fires 'delete'
+            else {
                 var elm = modelSelection.getSelectedElement();
-                if (elm && elm.is('element')) { // on the Model, bookmark is an Element (while on the View, bookmark is an attributeElement)
+                if (elm && elm.is('element')) {
                     if (elm.hasAttribute('name')) {
                         modelWriter.remove(elm);
                     }
                 }
             }
         });
-    }
-
-    refresh() {
-        this.isEnabled = true;// MUST always be true, otherwise the command cannot execute;
     }
 }
